75d81b66be263e79f0c3509ce18493db12da6072
Aiyappa Ganesh <aiyappa@b-eagles.com>
2013-11-13 16:26:17 +0530

    Community Page Update

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -1,24 +1,3 @@
-/*$(document).ready(function() {
-    $(window).scroll( function(){
-        var top_of_window = $(window).scrollTop();
-        var section_number = Math.ceil(top_of_window/1000);
-        var section = '#section'+section_number;
-        var next_section = '#section'+(section_number+1);
-        var new_y = -1 * (top_of_window%1000);
-        $(section).css({ 'background-position-y': new_y});
-        //$(next_section).css({ 'background-position-y': new_y+(section_number * 1000)});
-    });
-});*/
-
-/*$(document).ready(function() {
-    $("#landing section").each(function() {
-        var section_name = this.id;
-        var len = section_name.length;
-        var page = section_name.substring(len-1,len);
-        $(this).css({ 'top': (page-1)*1000});
-    });
-});*/
-
 function smoothScrollTo(event, link) {
     event.preventDefault();
     $.smoothScroll({
@@ -30,64 +9,47 @@ function smoothScrollTo(event, link) {
 
 var currentSectionNumber = 1;
 var totalDelta = 0;
+var acceptScroll = true;
 $(window).mousewheel(function(event, delta){
     event.preventDefault();
-    totalDelta = totalDelta + delta;
-    if(Math.abs(totalDelta) < 400) {
-        return;
-    } else {
-        totalDelta = 0;
-    }
-    oldSection = '#section'+currentSectionNumber;
-    newSection = null;
-    if (currentSectionNumber < 10 && delta < 0){
-        newSection = '#section'+(currentSectionNumber+1);
-        if(currentSectionNumber == 3 || currentSectionNumber == 5) {
-            $(oldSection+'a').animate({top: "-700px"}, 2000);
-            $(oldSection+'b').animate({top: "700px"}, 2000);
-        } else if(currentSectionNumber == 4) {
-            $(oldSection+'a').animate({top: "700px"}, 2000);
-            $(oldSection+'b').animate({top: "-700px"}, 2000);
+    if(acceptScroll) {
+        totalDelta = totalDelta + delta;
+        if(Math.abs(totalDelta) < 500) {
+            return;
+        } else {
+            acceptScroll = false;
         }
-        if(currentSectionNumber+1 != 4 && currentSectionNumber+1 != 5) {
-            if(currentSectionNumber+1 == 6) {
-                $('#section'+3).slideToggle();
-            } else {
-                $(oldSection).slideToggle();
+        oldSection = '#section'+currentSectionNumber;
+        newSection = null;
+        if (currentSectionNumber < 10 && delta < 0){
+            newSection = '#section'+(currentSectionNumber+1);
+            if(currentSectionNumber+1 == 3 || currentSectionNumber+1 == 4 || currentSectionNumber+1 == 5) {
+                $(newSection+'a').show().animate({top: "0px"}, 2000, function() {});
+                $(newSection+'b').show().animate({top: "0px"}, 2000);
             }
-        }
-        if(currentSectionNumber+1 == 3 || currentSectionNumber+1 == 4 || currentSectionNumber+1 == 5) {
-            $(newSection+'a').show().animate({top: "0px"}, 2000);
-            $(newSection+'b').show().animate({top: "0px"}, 2000);
-        }
-        if(currentSectionNumber+1 != 4 && currentSectionNumber+1 != 5) {
-            $(newSection).slideToggle();
-        }
-        currentSectionNumber = currentSectionNumber + 1;
-    } else if (currentSectionNumber > 1 && delta > 0) {
-        newSection = '#section'+(currentSectionNumber-1);
-        if(currentSectionNumber == 3 || currentSectionNumber == 5) {
-            $(oldSection+'a').animate({top: "-700px"}, 2000);
-            $(oldSection+'b').animate({top: "700px"}, 2000);
-        } else if(currentSectionNumber == 4) {
-            $(oldSection+'a').animate({top: "700px"}, 2000);
-            $(oldSection+'b').animate({top: "-700px"}, 2000);
-        }
-        if(currentSectionNumber-1 != 3 && currentSectionNumber-1 != 4) {
-            $(oldSection).slideToggle();
-        }
-        if(currentSectionNumber-1 == 3 || currentSectionNumber-1 == 4 || currentSectionNumber-1 == 5) {
-            $(newSection+'a').show().animate({top: "0px"}, 2000);
-            $(newSection+'b').show().animate({top: "0px"}, 2000);
-        }
-        if(currentSectionNumber-1 != 3 && currentSectionNumber-1 != 4) {
-            if(currentSectionNumber == 6) {
-                $('#section'+3).slideToggle();
-            } else {
+            if(currentSectionNumber+1 != 4 && currentSectionNumber+1 != 5) {
                 $(newSection).slideToggle();
             }
+            hideOldOnScrollDown(currentSectionNumber);
+            currentSectionNumber = currentSectionNumber + 1;
+        } else if (currentSectionNumber > 1 && delta > 0) {
+            newSection = '#section'+(currentSectionNumber-1);
+            if(currentSectionNumber-1 == 3 || currentSectionNumber-1 == 4 || currentSectionNumber-1 == 5) {
+                $(newSection+'a').show().animate({top: "0px"}, 2000);
+                $(newSection+'b').show().animate({top: "0px"}, 2000);
+            }
+            if(currentSectionNumber-1 != 3 && currentSectionNumber-1 != 4) {
+                if(currentSectionNumber == 6) {
+                    $('#section'+3).slideToggle();
+                } else {
+                    $(newSection).slideToggle();
+                }
+            }
+            hideOldOnScrollUp(currentSectionNumber);
+            currentSectionNumber = currentSectionNumber - 1;
         }
-        currentSectionNumber = currentSectionNumber - 1;
+        totalDelta = 0;
+        acceptScroll = true;
     }
 });
 
@@ -101,3 +63,55 @@ $(document).ready(function() {
         currentSectionNumber = sec_num;
     });
 });
+
+function hideOldOnScrollDown(currentSectionNumber) {
+    windowHeight = $(window).height();
+    if(currentSectionNumber == 3 || currentSectionNumber == 5) {
+        $(oldSection+'a').animate({top: -1*windowHeight}, 2000);
+        $(oldSection+'b').animate({top: windowHeight}, 2000);
+    } else if(currentSectionNumber == 4) {
+        $(oldSection+'a').animate({top: windowHeight}, 2000);
+        $(oldSection+'b').animate({top: -1*windowHeight}, 2000);
+    }
+    if(currentSectionNumber+1 != 4 && currentSectionNumber+1 != 5) {
+        if(currentSectionNumber+1 == 6) {
+            $('#section'+3).slideToggle();
+        } else {
+            $(oldSection).slideToggle();
+        }
+    }
+}
+
+function hideOldOnScrollUp(currentSectionNumber) {
+    windowHeight = $(window).height();
+    if(currentSectionNumber == 3 || currentSectionNumber == 5) {
+        $(oldSection+'a').animate({top: -1*windowHeight}, 2000);
+        $(oldSection+'b').animate({top: windowHeight}, 2000);
+    } else if(currentSectionNumber == 4) {
+        $(oldSection+'a').animate({top: windowHeight}, 2000);
+        $(oldSection+'b').animate({top: -1*windowHeight}, 2000);
+    }
+    if(currentSectionNumber-1 != 3 && currentSectionNumber-1 != 4) {
+        $(oldSection).slideToggle();
+    }
+}
+
+$(document).ready(function() {
+    $(window).on("resize load", resizeWindow);
+});
+
+function resizeWindow() {
+    windowHeight = $(window).height();
+    windowWidth = $(window).width();
+    $(".quarter-height").height(windowHeight*0.25);
+    $(".half-height").height(windowHeight*0.5);
+    $(".three-quarter-height").height(windowHeight*0.75);
+    $(".full-height").height(windowHeight);
+    $(".section6a-top").height(windowHeight*0.214);
+    $(".section6a-bottom").height(windowHeight*0.285);
+    $(".section6b-top").height(windowHeight*0.143);
+    $(".section6b-bottom").height(windowHeight*0.357);
+    $(".all-the-way-up").css('top',-1*windowHeight);
+    $(".all-the-way-down").css('top',windowHeight);
+    $(".half-way-across").css('left',windowWidth/2);
+}
