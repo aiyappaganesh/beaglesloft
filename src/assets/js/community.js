a20d5ae5c751238efe0ff0361b48254aaf6417c4
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-10-02 11:53:12 +0530

    Updates to newsletters page and beaglesloft mobile nav

--- src/assets/js/community.js
+++ src/assets/js/community.js
@@ -43,19 +43,27 @@ function getCurrentPage() {
 }
 
 function highlightLink(link) {
-    var accessed_page = link.substring(link.lastIndexOf('/'));
-    link_st_ind = link.indexOf('#section');
+    var link_st_ind = link.lastIndexOf('/');
+    var link_hash_ind = link.indexOf('#');
+    var accessed_page = link.substring(link_st_ind, link_hash_ind);
+    if(link_hash_ind == -1) {
+        accessed_page = link.substring(link_st_ind);
+    }
     $(".nav-link").each(function(){
-        st_ind = this.href.indexOf('#');
-        if((st_ind != -1) && (this.href.substring(st_ind) == link.substring(link_st_ind))) {
+        var st_ind = this.href.lastIndexOf('/');
+        var hash_ind = this.href.indexOf('#');
+        var curr_link_page = this.href.substring(st_ind, hash_ind);
+        if(hash_ind == -1) {
+            curr_link_page = this.href.substring(st_ind);
+        }
+        if(curr_link_page == accessed_page) {
             this.style.textDecoration="none";
             $(this).children().css({"color":"#fa7900"});
-        } else if(accessed_page == this.href.substring(this.href.lastIndexOf('/'))) {
-            this.style.textDecoration="none";
-            $(this).children().css({"color":"#fa7900"});
+            $(this).css({"color":"#ffffff"});
         } else {
             this.style.textDecoration="none";
             $(this).children().css({"color":"#ffffff"});
+            $(this).css({"color":"#adacac"});
         }
     });
 }
