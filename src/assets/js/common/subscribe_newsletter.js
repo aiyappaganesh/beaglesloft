35d4eb9ce9cba7da7031d93f5b39a14a2d954837
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-02-09 12:20:36 +0530

    Removing console logging

--- src/assets/js/common/subscribe_newsletter.js
+++ src/assets/js/common/subscribe_newsletter.js
@@ -15,8 +15,6 @@ function subscribe() {
     ga('send', 'event', 'Newsletter Subscription Page', 'click', 'Subscription Email Submitted');
     $.post('/api/common/subscribe_to_newsletter',{'email':$('#email').val(),'fname':$('#fname').val(),'lname':$('#lname').val()})
         .done(function(data){
-            console.log(data);
-            console.log(data.status);
             if(data.status == 'error') {
                 ga('send', 'event', 'Newsletter Subscription Page', 'login-fail', 'Subscription Failed');
                 $('#subscription-message').text('Error: '+data.name);
