a20a483fd580d54bb0488f38b6c02144af22ce6c
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-07-13 13:49:15 +0530

    Refactoring landing page services section handler

--- src/handlers/web/index.py
+++ src/handlers/web/index.py
@@ -10,88 +10,6 @@ import logging
 from util import mailjet
 from model.ui_models.factories.services import Services
 from model.ui_models.centered_contents import CenteredContents, CenteredContent
-from model.ui_models.carousel import Carousel
-from model.ui_models.image_carousel_slide import Image_Carousel_Slide
-
-def get_loft_images():
-    return ['/assets/img/landing/service_imgs/easychair.jpg','/assets/img/landing/service_imgs/terrace.jpg','/assets/img/landing/service_imgs/sunlight.jpg']
-
-def get_lofts_carousel():
-    id = "services-lofts"
-    classes = "service-carousel"
-    slides = [Image_Carousel_Slide(img) for img in get_loft_images()]
-    slide_classes= "landing-carousel-item"
-    indicator_classes= "landing-carousel-indicators"
-    lofts_carousel = Carousel(id, classes, slides, slide_classes, indicator_classes)
-    return lofts_carousel
-
-def get_program_images():
-    return ['/assets/img/landing/service_imgs/tb1.jpg','/assets/img/landing/service_imgs/tb2.jpg']
-
-def get_programs_carousel():
-    id = "services-programs"
-    classes = "service-carousel"
-    slides = [Image_Carousel_Slide(img) for img in get_program_images()]
-    slide_classes= "landing-carousel-item"
-    indicator_classes= "landing-carousel-indicators"
-    programs_carousel = Carousel(id, classes, slides, slide_classes, indicator_classes)
-    return programs_carousel
-
-def get_startup_images():
-    return ['/assets/img/landing/service_imgs/st1.jpg','/assets/img/landing/service_imgs/st2.jpg', '/assets/img/landing/service_imgs/st3.jpg']
-
-def get_startups_carousel():
-    id = "services-startups"
-    classes = "service-carousel"
-    slides = [Image_Carousel_Slide(img) for img in get_startup_images()]
-    slide_classes= "landing-carousel-item"
-    indicator_classes= "landing-carousel-indicators"
-    startups_carousel = Carousel(id, classes, slides, slide_classes, indicator_classes)
-    return startups_carousel
-
-def get_community_images():
-    return ['/assets/img/landing/service_imgs/cm1.jpg','/assets/img/landing/service_imgs/cm2.jpg']
-
-def get_community_carousel():
-    id = "services-community"
-    classes = "service-carousel"
-    slides = [Image_Carousel_Slide(img) for img in get_community_images()]
-    slide_classes= "landing-carousel-item"
-    indicator_classes= "landing-carousel-indicators"
-    community_carousel = Carousel(id, classes, slides, slide_classes, indicator_classes)
-    return community_carousel
-
-def get_event_images():
-    return ['/assets/img/landing/service_imgs/ev2.jpg','/assets/img/landing/service_imgs/ev3.jpg', '/assets/img/landing/service_imgs/ev1.jpg']
-
-def get_events_carousel():
-    id = "services-events"
-    classes = "service-carousel"
-    slides = [Image_Carousel_Slide(img) for img in get_event_images()]
-    slide_classes= "landing-carousel-item"
-    indicator_classes= "landing-carousel-indicators"
-    events_carousel = Carousel(id, classes, slides, slide_classes, indicator_classes)
-    return events_carousel
-
-def get_services_list():
-    services = [
-        ('/assets/img/landing/lofts.png','/assets/img/landing/lofts_dark.png','LOFTS', get_lofts_carousel()),
-        ('/assets/img/landing/programs.png','/assets/img/landing/programs_dark.png','PROGRAMS', get_programs_carousel()),
-        ('/assets/img/landing/startups.png','/assets/img/landing/startups_dark.png','STARTUPS', get_startups_carousel()),
-        ('/assets/img/landing/community.png','/assets/img/landing/community_dark.png','COMMUNITY', get_community_carousel()),
-        ('/assets/img/landing/events.png','/assets/img/landing/events_dark.png','EVENTS', get_events_carousel())
-    ]
-    return services
-
-def get_value_to_services_centered_contents():
-    centered_contents = []
-    contents_arr = [("We help IT Services companies significantly increase the revenue per employee",["header-2","black-font"])]
-    contents = [CenteredContent(s[0], s[1]) for s in contents_arr]
-    centered_contents.append(CenteredContents(360, 0, contents))
-    contents_arr = [("We reduce your attrition by up to 30%",["header-2","black-font"])]
-    contents = [CenteredContent(s[0], s[1]) for s in contents_arr]
-    centered_contents.append(CenteredContents(256, 0, contents))
-    return centered_contents
 
 def get_why_beaglesloft_centered_contents():
     centered_contents = []
@@ -108,15 +26,13 @@ def get_why_beaglesloft_centered_contents():
     centered_contents.append(CenteredContents(308, 0, contents))
     return centered_contents
 
-
 class IndexPage(WebRequestHandler):
     def get(self):
         path = 'landing.html'
         template_values = {'is_member': True if 'member' in self.session else False}
         if 'member' in self.session:
             template_values['member'] = Member.get_member_json(self.session['member'])
-        template_values['services'] = Services.get_services(get_services_list())
-        template_values['value_to_services_centered'] = get_value_to_services_centered_contents()
+        template_values['services'] = Services.get_services()
         template_values['why_beaglesloft_centered'] = get_why_beaglesloft_centered_contents()
         self.render_template(template_name=None, template_values=template_values)
 
--- src/model/ui_models/factories/services.py
+++ src/model/ui_models/factories/services.py
@@ -1,9 +1,45 @@
 from model.ui_models.service import Service
+from model.ui_models.carousel import Carousel
+from model.ui_models.image_carousel_slide import Image_Carousel_Slide
+
+lofts = 'lofts'
+programs = 'programs'
+startups = 'startups'
+community = 'community'
+events = 'events'
+
+services_list = [lofts, programs, startups, community, events]
+
+def get_images(service):
+    images = {
+        lofts: ['/assets/img/landing/service_imgs/easychair.jpg','/assets/img/landing/service_imgs/terrace.jpg','/assets/img/landing/service_imgs/sunlight.jpg'],
+        programs: ['/assets/img/landing/service_imgs/tb1.jpg','/assets/img/landing/service_imgs/tb2.jpg'],
+        startups: ['/assets/img/landing/service_imgs/st1.jpg','/assets/img/landing/service_imgs/st2.jpg', '/assets/img/landing/service_imgs/st3.jpg'],
+        community: ['/assets/img/landing/service_imgs/cm1.jpg','/assets/img/landing/service_imgs/cm2.jpg'],
+        events: ['/assets/img/landing/service_imgs/ev2.jpg','/assets/img/landing/service_imgs/ev3.jpg', '/assets/img/landing/service_imgs/ev1.jpg']
+    }
+    return images[service]
+
+def get_images_carousel(id, images, classes=None, slide_classes=None, indicator_classes=None):
+    if not classes:
+        classes = "service-carousel"
+    if not slide_classes:
+        slide_classes= "landing-carousel-item"
+    if not indicator_classes:
+        indicator_classes= "landing-carousel-indicators"
+    slides = [Image_Carousel_Slide(img) for img in images]
+    carousel = Carousel(id, classes, slides, slide_classes, indicator_classes)
+    return carousel
+
+def get_services_list():
+    services = [('/assets/img/landing/'+service+'.png','/assets/img/landing/'+service+'_dark.png',service,get_images_carousel('services-'+service,get_images(service))) for service in services_list]
+    return services
 
 class Services():
     @classmethod
-    def get_services(self, services):
+    def get_services(self):
         ret_val = []
+        services = get_services_list()
         for service in services:
             s = Service(service[0], service[1], service[2], service[3])
             ret_val.append(s)
