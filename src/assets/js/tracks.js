c97ca792a9445655fd2fa25af4aded4e9fc2cecb
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-07-13 14:54:20 +0530

    Updating Program Listing Page

--- src/assets/js/tracks.js
+++ src/assets/js/tracks.js
@@ -2,3 +2,30 @@ function showTrackPrograms(id){
     $('.track-program-listing').css('display', 'none');
     $(id).css('display', 'block');
 }
+
+var chevToggle = true;
+
+function toggleChevron(e) {
+    if($(e).hasClass('rotate-180')) {
+        $(e).removeClass('rotate-180');
+    } else {
+        $(e).addClass('rotate-180');
+    }
+    chevToggle = true;
+}
+
+$('.collapse').on('show.bs.collapse', function() {
+    if(chevToggle) {
+        var glyph = $(this).parent().find('.glyphicon-chevron-down');
+        chevToggle = false;
+        toggleChevron(glyph);
+    }
+});
+
+$('.collapse').on('hide.bs.collapse', function() {
+    if(chevToggle) {
+        var glyph = $(this).parent().find('.glyphicon-chevron-down');
+        chevToggle = false;
+        toggleChevron(glyph);
+    }
+});
