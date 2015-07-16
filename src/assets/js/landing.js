c2459176f3f2260ee5d5de3994809beb0689882b
Ashray <ashray.velapanur@gmail.com>
2015-07-16 16:36:28 +0530

    made the logo on the landing page fade in and out

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -1,11 +1,8 @@
 
 $(document).ready(function(){
-	$('#starting-animation-bg .centered-fullscreen').fadeOut(4000, function(){ $('#starting-animation-bg').remove(); });
-	/*
-    $('#starting-animation-bg').delay(20000).queue(
-        function(){
-            $(this).remove();
-        }
-    );
-	*/
+	$('#starting-animation-bg .centered-fullscreen .logo-container').fadeIn(2000, function() {
+		$('#starting-animation-bg .centered-fullscreen').fadeOut(2000, function() {
+			$('#starting-animation-bg').remove();
+		});	
+	});
 });
