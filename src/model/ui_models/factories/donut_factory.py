b7b4b7a75619d947eca5ae0e2afca0b63f1913c8
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-07-28 16:55:55 +0530

    Adding track progress accordians on the tracks page

--- src/model/ui_models/factories/donut_factory.py
+++ src/model/ui_models/factories/donut_factory.py
@@ -4,6 +4,6 @@ class DonutFactory():
 	def get_donuts(cls, size, cutout, scores, container_color, full_color, empty_color):
 		ret_val = []
 		for score in scores:
-			d = Donut(size, cutout, score[0], score[1], container_color, full_color, empty_color)
+			d = Donut(size, cutout, score[0], score[1], container_color, full_color, empty_color, score[2] if len(score)>2 else None)
 			ret_val.append(d)
 		return ret_val
