18b7d2692096daf65f3bf59e52b73a2d5bbeb252
Ashray <ashray.velapanur@gmail.com>
2015-07-29 15:22:44 +0530

    wip - using css for mouseover highlighting

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -26,11 +26,13 @@ $(document).ready(function(){
     $('.tab_icons .tab').click(
                 function(){
                     var carousel_id = $(this).children('input[name="carousel_id"]').first().val();
-                    $('.active_tab h3').css('color', '#c62530');
-                    $('.active_tab a img').attr('src', '/assets/img/tracks/'+ $('.active_tab #carousel_id').val().replace('#','') +'.png')
+                    $('.active_tab h3').css('color', '');
+                    var icon_url = $('.active_tab #icon_url').val()
+                    $('.active_tab a #icon').attr('src', icon_url);
                     $('.active_tab').removeClass('active_tab');
                     $(carousel_id + '_tab').addClass('active_tab');
-                    $(carousel_id + '_tab a img').attr('src', '/assets/img/tracks/'+ carousel_id.replace('#','') +'_dark.png')
+                    var highlight_icon_url = $(carousel_id +'_tab #highlight_icon_url').val();
+                    $(carousel_id + '_tab a img').attr('src', highlight_icon_url)
                     $(carousel_id + '_tab h3').css('color', '#000000');
                     $('.service-carousel').fadeOut(0);
                     $(carousel_id).fadeIn(0);
