de71a836ec1f301940970db487f9dd68888d92ca
aakaashh <aakaashh.s@gmail.com>
2015-07-31 18:15:07 +0530

    The colour of tab icons replicate the selected tab icon

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -7,8 +7,16 @@ $(document).ready(function(){
 	});
 
 	$('.tab_icon').click(function(){	
-		console.log();
         var carousel_id = $(this).children('input[name="carousel_id"]').first().val();
+        $('.active_tab').removeClass('first_icon');
+        $('.active_tab h3').css('color', '');
+        var icon_url = $('.active_tab #icon_url').val()
+        $('.active_tab a #icon').attr('src', icon_url);
+        $('.active_tab').removeClass('active_tab');
+        $(carousel_id + '_tab').addClass('active_tab');
+        var highlight_icon_url = $(carousel_id +'_tab #highlight_icon_url').val();
+        $(carousel_id + '_tab a img').attr('src', highlight_icon_url)
+        $(carousel_id + '_tab h3').css('color', '#000000');                    
         $('.service-carousel').fadeOut(0);
         $(carousel_id).fadeIn(0);
     });
