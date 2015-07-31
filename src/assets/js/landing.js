80462b0193021b94ee90bf184778651d770b69d7
aakaashh <aakaashh.s@gmail.com>
2015-07-31 17:00:31 +0530

    added the functionality of showing the track when clicked on the respective tab icon

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
 			$('#starting-animation-bg').remove();
 		});	
 	});
-});
+
+	$('.tab_icon').click(function(){	
+		console.log();
+        var carousel_id = $(this).children('input[name="carousel_id"]').first().val();
+        $('.service-carousel').fadeOut(0);
+        $(carousel_id).fadeIn(0);
+    });
+});    
