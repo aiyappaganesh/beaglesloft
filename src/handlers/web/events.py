66c8a59c51ddc5992366b5a7cf8a71e8bc5e9354
aakaashh <aakaashh.s@gmail.com>
2015-08-03 15:27:08 +0530

    Changed handler to take events page template from events.html. Moved hero class as mixin to standard partial. Created a new sass file for events page

--- src/handlers/web/events.py
+++ src/handlers/web/events.py
@@ -16,7 +16,7 @@ class EventsPage(WebRequestHandler):
         template_values['is_member'] = True if 'member' in self.session else False
         if 'member' in self.session:
             template_values['member'] = Member.get_member_json(self.session['member'])
-        self.render_template(template_name=None, template_values=template_values)
+        self.render_template(template_name='events.html', template_values=template_values)
 
 class CreateEventPage(WebRequestHandler):
     def get(self):
