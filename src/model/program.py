3fd0fe20916d11c40cbac478a6c430266d35bf54
Ashray <ashray.velapanur@gmail.com>
2015-08-04 14:20:56 +0530

    added some properties to program db model

--- src/model/program.py
+++ src/model/program.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import db
+from datetime import datetime
 
 class Program(db.Model):
     name = db.StringProperty(indexed=False)
@@ -6,3 +7,15 @@ class Program(db.Model):
     image = db.StringProperty(indexed=False)
     slots = db.IntegerProperty(indexed=False)
     start_date = db.DateTimeProperty(indexed=False)
+
+    @property
+    def id(self):
+        return self.key().name()
+
+    @property
+    def url(self):
+        return '/tracks/program_listing?id=' + self.id
+
+    @property
+    def formatted_start_date(self):
+        return self.start_date.strftime('%b %d %Y')
