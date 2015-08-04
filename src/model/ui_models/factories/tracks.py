ae76b50f928684b19c922efbed3a82eff3d80bcb
Ashray <ashray.velapanur@gmail.com>
2015-08-04 14:21:41 +0530

    changed start date strings

--- src/model/ui_models/factories/tracks.py
+++ src/model/ui_models/factories/tracks.py
@@ -57,10 +57,10 @@ def get_programs(track):
                 Program('App Analytics and Audience Engagement', 'Aug 1 2015', 25, mobile_track, 'Deep dive into analytics, data monitoring, audience segmentation and engagement.', 'analytics', get_modules_for(mobile_track)),
                 Program('App Store Optimization', 'Aug 21 2015', 15, mobile_track, 'How to launch your app effectively and optimize for success in the app store.', 'appstore_opt', get_modules_for(mobile_track)),
                 Program('User acquisition', 'Aug 28 2015', 20, mobile_track, 'Learn how to go from zero to the first thousand users to prove product market fit.', 'useracquisition', get_modules_for(mobile_track)),
-                Program('Android Programming 101', 'Sept 5 2015', 20, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'android_programming', get_modules_for(mobile_track)),
-                Program('Building an Android App', 'Sept 12 2015', 20, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'building_an_android_app', get_modules_for(mobile_track)),
-                Program('High quality Android code', 'Sept 19 2015', 25, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'android_coding', get_modules_for(mobile_track)),
-                Program('Android App Analytics and Audience Engagement', 'Sept 26 2015', 20, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'analytics', get_modules_for(mobile_track)),
+                Program('Android Programming 101', 'Sep 5 2015', 20, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'android_programming', get_modules_for(mobile_track)),
+                Program('Building an Android App', 'Sep 12 2015', 20, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'building_an_android_app', get_modules_for(mobile_track)),
+                Program('High quality Android code', 'Sep 19 2015', 25, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'android_coding', get_modules_for(mobile_track)),
+                Program('Android App Analytics and Audience Engagement', 'Sep 26 2015', 20, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'analytics', get_modules_for(mobile_track)),
                 Program('Google Play Store Optimization', 'Oct 1 2015', 25, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'google_play', get_modules_for(mobile_track)),
                 Program('Android user acquisition', 'Oct 8 2015', 10, mobile_track, 'Build an iOS App from scratch with a deep dive into iOS Programming.', 'useracquisition', get_modules_for(mobile_track))
             ],
@@ -68,32 +68,32 @@ def get_programs(track):
                    Program('Going beyond HTML and CSS', 'Aug 9 2015', 20, web_track, 'Learn how to write D.R.Y code and the frameworks and libraries to achieve that.', 'less', get_modules_for(beyond_html)),
                    Program('Mathematics for Computer Science', 'Aug 23 2015', 30, web_track, 'The Calculus, Statistics, Combinatorics, Graph Theory, Probablity and number theory every data scientist should know.', 'cont_math', get_modules_for(math_for_cs)),
                    Program('Algorithms and Datastructures revisited', 'Aug 30 2015', 20, web_track, 'Computer science fundamentals which separates a good programmer from the best.', 'algo', get_modules_for(algs_and_ds)),
-                   Program('Distributed computing', 'Sept 7 2015', 25, web_track, 'Basic concepts of distributed computing and how to execute it on the cloud.', 'dist', get_modules_for(distributed_computing)),
-                   Program('Non-relational Databases', 'Sept 14 2015', 30, web_track, 'The new Non-relational mathematics and how commercial databases implement them.', 'no_sql', get_modules_for(non_relational_databases)),
-                   Program('NLP 101', 'Sept 21 2015', 25, web_track, 'Tools and frameworks for processing and mining data from natural language text.', 'nlp', get_modules_for(nlp)),
-                   Program('Big Data 101', 'Sept 28 2015', 20, web_track, 'Learn how to store massive amounts of data in distributed databases and process them using MapReduce.', 'big_data', get_modules_for(big_data))],
+                   Program('Distributed computing', 'Sep 7 2015', 25, web_track, 'Basic concepts of distributed computing and how to execute it on the cloud.', 'dist', get_modules_for(distributed_computing)),
+                   Program('Non-relational Databases', 'Sep 14 2015', 30, web_track, 'The new Non-relational mathematics and how commercial databases implement them.', 'no_sql', get_modules_for(non_relational_databases)),
+                   Program('NLP 101', 'Sep 21 2015', 25, web_track, 'Tools and frameworks for processing and mining data from natural language text.', 'nlp', get_modules_for(nlp)),
+                   Program('Big Data 101', 'Sep 28 2015', 20, web_track, 'Learn how to store massive amounts of data in distributed databases and process them using MapReduce.', 'big_data', get_modules_for(big_data))],
         designer:[
                 Program('Design Thinking', 'Aug 3 2015', 15, designer, 'Learn the methods and approaches to use design as a "way of thinking" for creative action.', 'design_thinking', None),
                 Program('iOS App Design', 'Aug 10 2015', 20, designer, 'From using 3D depth to layered design. Learn the design concepts required to make world class iOS Apps.', 'ios_design', None),
                 Program('Android App Design', 'Aug 17 2015', 10, designer, """An in-depth look at Android's implementation of "material design" and how you can use it make elegant Android Apps.""", 'android_design', None),
                 Program('Web Design', 'Aug 24 2015', 15, designer, "Parallax, Single Scroll pages, Sticky menus and other new age design paradigms which is shaping today's web pages.", 'web_design', None),
-                Program('Responsive Design', 'Sept 3 2015', 20, designer, 'Learn how to make your website render seamlessly across a multitude of devices and resolutions.', 'responsive_design', None),
-                Program('Branding Concepts', 'Sept 10 2015', 20, designer, 'How to turn an idea into a product and brand that people love.', 'branding', None)
+                Program('Responsive Design', 'Sep 3 2015', 20, designer, 'Learn how to make your website render seamlessly across a multitude of devices and resolutions.', 'responsive_design', None),
+                Program('Branding Concepts', 'Sep 10 2015', 20, designer, 'How to turn an idea into a product and brand that people love.', 'branding', None)
             ],
         product_manager:[
             Program('The new age Product Manager', 'Aug 4 2015', 10, product_manager, 'Learn to handle challenges in software development with ever changing requirements and faster iterations.', 'product', None),
             Program('Tools for Product Management', 'Aug 11 2015', 15, product_manager, 'Master new age product management tools like Trello, Typeform and Quip to make you more productive and agile.', 'tools', None),
             Program('Design Thinking for Management', 'Aug 18 2015', 10, product_manager, 'Integrate pillars of design thinking- desirability, feasibility and viability into product management.', 'design_thinking', None),
             Program('Agile based Product Management', 'Aug 25 2015', 20, product_manager, 'Embrace changes and make it work to your favor through agile based product management.', 'agile', None),
-            Program('Client Management', 'Sept 1 2015', 20, product_manager, 'Learn all the tricks of the trade to develop a long lasting and happy relationship with your clients.', 'client_managament', None),
-            Program('Lean Product Management', 'Sept 8 2015', 15, product_manager, 'Learn to work in iterations to build, measure and learn only what is required and save valuable resources.', 'product_management', None)
+            Program('Client Management', 'Sep 1 2015', 20, product_manager, 'Learn all the tricks of the trade to develop a long lasting and happy relationship with your clients.', 'client_managament', None),
+            Program('Lean Product Management', 'Sep 8 2015', 15, product_manager, 'Learn to work in iterations to build, measure and learn only what is required and save valuable resources.', 'product_management', None)
             ],
         founder: [
             Program('Bootstrapping your startup', 'Aug 5 2015', 10, founder, 'Make the most of your own resources to build a startup without an outside investor', 'bootstrap', None),
             Program('Raising the seed round', 'Aug 12 2015', 15, founder, 'Learn the nuances of acquiring a seed round investment to shape up your idea', 'seed', None),
             Program('Raising the Series A round', 'Aug 19 2015', 10, founder, 'Obtain the skills it takes to get the best term sheet from an investor at the most crucial stage of your startup', 'series-a', None),
             Program('MVP and Product Market Fit', 'Aug 26 2015', 25, founder, 'Decide the right features that goes into your MVP to produce highest return on investment', 'mvp', None),
-            Program('Investor Management', 'Sept 2 2015', 20, founder, 'Understand how to choose investors and strategize accepting funds', 'investor', None)
+            Program('Investor Management', 'Sep 2 2015', 20, founder, 'Understand how to choose investors and strategize accepting funds', 'investor', None)
             ]
     }
     return programs[track]
