c4eb4e792084541e0f1c86a57bb9b38e41e1ab8e
Ashray <ashray.velapanur@gmail.com>
2015-08-06 14:28:46 +0530

    added start date to program modules

--- src/model/program_module.py
+++ src/model/program_module.py
@@ -3,5 +3,4 @@ from google.appengine.ext import db
 class ProgramModule(db.Model):
     name = db.StringProperty(indexed=True)
     units = db.StringListProperty(indexed=False)
-
-
+    start_date = db.DateTimeProperty(indexed=False)
